bf51d533124a228563b46b1f75c0be00f110797a
Konstantin <nemeshkv@gmail.com>
2022-06-12 16:04:07 +0500

    new card VisaGoldCard

--- src/ru/nemesh/Main.java
+++ src/ru/nemesh/Main.java
@@ -2,11 +2,17 @@ package ru.nemesh;
 
 public class Main {
     public static void main(String[]  args) {
+        int amount = 500;
         VisaClassicCard card = new VisaClassicCard();
         card.setId("1234567890");
         card.setCardHolder("Konstantin");
         card.setDiscount(0.5);
-        int amount = 500;
+
+        VisaGoldCard cardgold = new VisaGoldCard();
+        card.setId("11111111111");
+        card.setCardHolder("Vladimir");
+        card.setDiscount(1);
+
     }
         static int getCurentDiscount(int amount, double setDiscount){
            return (int) setDiscount;
