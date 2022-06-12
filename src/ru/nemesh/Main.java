06d0fa3da87721e0230f315ad97e71f29781a387
Konstantin <nemeshkv@gmail.com>
2022-06-12 21:52:28 +0500

    test amount

--- src/ru/nemesh/Main.java
+++ src/ru/nemesh/Main.java
@@ -2,18 +2,18 @@ package ru.nemesh;
 
 public class Main {
     public static void main(String[] args) {
-        int amount = 500;
         VisaClassicCard card = new VisaClassicCard();
         card.setId("1234567890");
         card.setCardHolder("Konstantin");
         card.setDiscount(0.5);
+        card.setAmount(500);
 
         VisaGoldCard cardgold = new VisaGoldCard();
         card.setId("11111111111");
         card.setCardHolder("Vladimir");
         card.setDiscount(1);
-        card.getCurrentDiscount(amount);
-        System.out.println("Кэшбэк по карте " + "Konstantin" + " от суммы "  + amount + " составил " + card.resultDiscount + " рублей");
+        card.getCurrentDiscount(500);
+        System.out.println("Кэшбэк по карте " + "Konstantin" + " от суммы "  + card.amount + " составил " + card.resultDiscount + " рублей");
         return;
     }
 }
--- src/ru/nemesh/VisaClassicCard.java
+++ src/ru/nemesh/VisaClassicCard.java
@@ -1,10 +1,10 @@
 package ru.nemesh;
 
 public class VisaClassicCard {
-    public int resultDiscount;
     private String id;
     private String cardHolder;
-    int discount;
+    public int resultDiscount;
+    public double discount;
     public int amount;
 
     public void setId(String id) {
@@ -13,12 +13,11 @@ public class VisaClassicCard {
     public void setCardHolder(String cardHolder){
         this.cardHolder = cardHolder;
     }
-    public int setDiscount(double setDiscount) { this.discount = discount;
-        return discount;
-    }
+    public void setDiscount(double setDiscount) { this.discount = discount;}
+    public void setAmount(int setAmount) { this.amount = amount;}
 
-    public void getCurrentDiscount(int amount) {
-        resultDiscount = amount*discount;
+    public void getCurrentDiscount(int resultDiscount) {
+        resultDiscount = (int) (this.amount *discount);
         return;
     }
 }
